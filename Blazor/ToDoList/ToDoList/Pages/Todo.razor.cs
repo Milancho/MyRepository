3f802f78e8f43ece98d2e094fe491349d478dd45
Milancho <milancho.arsovski@gmail.com>
2020-03-20 12:56:49 +0100

    add Blazor Server app

---
+++ Blazor/ToDoList/ToDoList/Pages/Todo.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Pages
+{
+    public partial class Todo
+    {
+        private IList<TodoItem> todos = new List<TodoItem>();
+        private string newTodo;
+        private void AddTodo()
+        {
+            if (!string.IsNullOrEmpty(newTodo))
+            {
+                todos.Add(new TodoItem() { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
